455cd89eb703a173a1d8cd5522b88362536c2b37
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2022-01-14 21:28:06 +0100

    feat: first commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express, { Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.get('/', (req: Request, res: Response) => {
-      res.json({message: 'Hello World!'})
-})
+  res.json({ message: 'Hello World!' });
+});
 
 if (import.meta.env.PROD) {
   app.listen(3000);
 }
 
 export const viteNodeApp = app;
-
